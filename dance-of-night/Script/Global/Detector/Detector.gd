f2df2fff435f06ad57a5c39215642ccea64b2d89
zhngsnl-cell <zhngsnl@gmail.com>
2026-05-22 10:48:41 +0800

    完成演奏系统

--- dance-of-night/Script/Global/Detector/Detector.gd
+++ dance-of-night/Script/Global/Detector/Detector.gd
@@ -36,6 +36,16 @@ var pitch_state:Array[bool] = [
 	false,
 	false,
 ]
+var pitch_rank:Array[int] = [
+	0,
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+]
 var time_container:Array[float]
 var pitch_container:Array[int]
 var length_container:Array[float]
@@ -46,6 +56,10 @@ var delay:float = 0.2
 @export var sound_player:AudioStreamPlayer
 @export var music_score:TimeLine
 
+func push_to_front(the_pitch:int,the_pitch_rank:Array[int])->void:
+	the_pitch_rank.erase(the_pitch)
+	the_pitch_rank.push_front(the_pitch)
+
 func calculate_pitch()->void:
 	for i:int in range(8):
 		print(pow(2.0,float(i)/6.0))
@@ -54,41 +68,49 @@ func change_pitch_state()->void:
 	if Input.is_action_just_pressed("key_a"):
 		pitch_state[0] = true
 		current_pitch = 0
+		push_to_front(0,pitch_rank)
 	elif Input.is_action_just_released("key_a"):
 		pitch_state[0] = false
 	if Input.is_action_just_pressed("key_s"):
 		pitch_state[1] = true
 		current_pitch = 1
+		push_to_front(1,pitch_rank)
 	elif Input.is_action_just_released("key_s"):
 		pitch_state[1] = false
 	if Input.is_action_just_pressed("key_d"):
 		pitch_state[2] = true
 		current_pitch = 2
+		push_to_front(2,pitch_rank)
 	elif Input.is_action_just_released("key_d"):
 		pitch_state[2] = false
 	if Input.is_action_just_pressed("key_f"):
 		pitch_state[3] = true
 		current_pitch = 3
+		push_to_front(3,pitch_rank)
 	elif Input.is_action_just_released("key_f"):
 		pitch_state[3] = false
 	if Input.is_action_just_pressed("key_j"):
 		pitch_state[4] = true
 		current_pitch = 4
+		push_to_front(4,pitch_rank)
 	elif Input.is_action_just_released("key_j"):
 		pitch_state[4] = false
 	if Input.is_action_just_pressed("key_k"):
 		pitch_state[5] = true
 		current_pitch = 5
+		push_to_front(5,pitch_rank)
 	elif Input.is_action_just_released("key_k"):
 		pitch_state[5] = false
 	if Input.is_action_just_pressed("key_l"):
 		pitch_state[6] = true
 		current_pitch = 6
+		push_to_front(6,pitch_rank)
 	elif Input.is_action_just_released("key_l"):
 		pitch_state[6] = false
 	if Input.is_action_just_pressed("key_semi"):
 		pitch_state[7] = true
 		current_pitch = 7
+		push_to_front(7,pitch_rank)
 	elif Input.is_action_just_released("key_semi"):
 		pitch_state[7] = false
 
@@ -101,10 +123,13 @@ func pressing()->bool:
 func play_sound()->void:
 	if pressing():
 		if pitch_state[current_pitch] == true:
-			sound_player.pitch_scale = current_pitch
+			sound_player.pitch_scale = pitch_scale[current_pitch]
 		else:
+			for i:int in pitch_rank:
+				if pitch_state[i] == true:
+					sound_player.pitch_scale = pitch_scale[i]
+					break
 			#创建动态数组，每次按下一个键就把那个键提到数组最前方，如果松开了键，就按数组从前向后遍历决定谁演奏
-			sound_player.pitch_scale = last_pitch
 		if sound_player.playing == false:
 			sound_player.play()
 	else:
